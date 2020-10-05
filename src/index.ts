bbc66713febc43943728d31aa8567c786d31df25
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-10-05 09:35:35 -0700

    Cleaned up unnecessary imports.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { fromJS } from "knockout.mapping";
-
 export * from "./logging/appInsightsLogger";
 export * from "./persistence/azureBlobStorage";
 export * from "./appInsights.publish.module";
